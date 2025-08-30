3f289dbef441fe63c9552186fb628eb2674f9455
luisbedoyagt <90284786+luisbedoyagt@users.noreply.github.com>
2025-08-30 12:17:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -130,18 +130,23 @@ function displayUpcomingEvents() {
     if (allData.calendario) {
         for (const liga in allData.calendario) {
             allData.calendario[liga].forEach(event => {
-                // Parsea la fecha y hora
-                const startDate = new Date(event.fecha);
-                const endDate = new Date(event.hora);
+                // Combina la fecha y la hora en un solo string
+                const combinedDateTime = `${event.fecha}T${event.hora}:00`;
+                const eventDate = new Date(combinedDateTime);
+
+                // Verifica si la fecha es válida
+                if (isNaN(eventDate)) {
+                    console.error('Fecha inválida para el evento:', event);
+                    return; // Salta este evento si la fecha no es válida
+                }
 
                 // Formatea la fecha y hora a un formato legible
                 const dateOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
                 const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: false, timeZone: 'America/Guatemala' };
 
-                const formattedDate = startDate.toLocaleDateString('es-ES', dateOptions);
-                const formattedTime = startDate.toLocaleTimeString('es-ES', timeOptions);
+                const formattedDate = eventDate.toLocaleDateString('es-ES', dateOptions);
+                const formattedTime = eventDate.toLocaleTimeString('es-ES', timeOptions);
                 
-                // Muestra solo la fecha de inicio, ya que la de finalización es un valor predeterminado
                 const eventDateTime = `${formattedDate} ${formattedTime} (GT)`;
                 
                 allEvents.push({
